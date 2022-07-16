80d8f411bd37a78a0008e1614f9645d7f9def710
ikuvamzz <76558264+ikuvamzz@users.noreply.github.com>
2022-07-16 22:59:57 +0530

    Update Linux_QemuWindows10ColabNoVT-X.sh

--- Linux_QemuWindows10ColabNoVT-X.sh
+++ Linux_QemuWindows10ColabNoVT-X.sh
@@ -1,3 +1,7 @@
+useradd -m user
+adduser user sudo
+echo 'user:root' | sudo chpasswd
+sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 read -p "Paste auth here (Copy and Ctrl+V to paste then press Enter): " CRP
 echo "Please wait for installing..."
 sudo apt update -y > /dev/null 2>&1
@@ -8,7 +12,10 @@ curl -L -o lite10.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCO
 echo "Windows 10 x64 Lite On Google Colab"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=8  -m 10000M -machine usb=on -device usb-tablet > /dev/null 2>&1
+
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg -i chrome
-sudo apt-get install -f
-$CRP
+dpkg --install chrome-remote-desktop_current_amd64.deb
+apt install --assume-yes --fix-broken
+adduser user chrome-remote-desktop
+su - user -c $CRP --pin=123456
+service chrome-remote-desktop start
